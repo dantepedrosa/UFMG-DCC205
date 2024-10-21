6b0ce6e46a94e10801aaa89c5de4cabe04466fba
Dante Pedrosa <dantepedrosa@gmail.com>
2024-10-21 11:04:10 -0300

    Mat.h criado e comentado com formato doxygen

---
+++ ap1 - matrix operator/include/mat.h
@@ -0,0 +1,140 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <getopt.h>
+
+#define avisoAssert(e,m) ((void) ((e)?0: __avisoassert (#e,__FILE__,__LINE__,m)))
+#define __avisoassert(e,file,line,m) ((void) fprintf(stderr,"%s:%u: Aviso: '%s' - %s\n",file,line,e,m),0)
+#define erroAssert(e,m) ((void) ((e)?0: __erroassert (#e,__FILE__,__LINE__,m)))
+#define __erroassert(e,file,line,m) ((void) fprintf(stderr,"%s:%u: Erro '%s' - %s\n",file,line,e,m), abort(),0)
+
+#define MAXTAM 5
+
+/**
+ * @brief Estrutura que representa uma matriz.
+ * 
+ * @struct mat
+ * @var mat::m Matriz de valores do tipo double.
+ * @var mat::tamx Número de linhas.
+ * @var mat::tamy Número de colunas.
+ * @var mat::id Identificador único.
+ */
+typedef struct mat{
+    double m[MAXTAM][MAXTAM];
+    int tamx, tamy;
+    int id;
+} mat_tipo;
+
+// Limite superior da inicializacao aleatoria
+#define INITRANDOMRANGE 10
+// Macro que realiza swap sem variavel auxiliar */
+#define ELEMSWAP(x,y) (x+=y,y=x-y,x-=y)
+
+/**
+ * @brief Cria matriz com dimensoes tx X ty
+ * 
+ * @param mat Ponteiro para a matriz
+ * @param tx Numero de linhas
+ * @param ty Numero de colunas
+ * @param id Identificador da matriz
+ */
+void criaMatriz(mat_tipo * mat, int tx, int ty, int id);
+
+/**
+ * @brief Inicializa mat com valores nulos
+ * 
+ * @param mat Ponteiro para a matriz
+ */
+void inicializaMatrizNula(mat_tipo * mat);
+
+/**
+ * @brief Inicializa mat com valores aleatorios
+ * 
+ * @param mat Ponteiro para a matriz
+ */
+void inicializaMatrizAleatoria(mat_tipo * mat);
+
+/**
+ * @brief Acessa mat para fins de registro de acesso
+ * 
+ * @param mat Ponteiro para a matriz
+ * @return double Valor acessado
+ */
+double acessaMatriz(mat_tipo * mat);
+
+/**
+ * @brief Imprime a matriz com a identificacao de linhas e colunas
+ * 
+ * @param mat Ponteiro para a matriz
+ */
+void imprimeMatriz(mat_tipo * mat);
+
+/**
+ * @brief Salva a matriz em arquivo
+ * 
+ * @param mat Ponteiro para a matriz
+ * @param out Ponteiro para o arquivo de saida
+ */
+void salvaMatriz(mat_tipo * mat, FILE * out);
+
+/**
+ * @brief Atribui o valor v ao elemento (x,y) de mat
+ * 
+ * @param mat Ponteiro para a matriz
+ * @param x Linha do elemento
+ * @param y Coluna do elemento
+ * @param v Valor a ser atribuido
+ */
+void escreveElemento(mat_tipo * mat, int x, int y, double v);
+
+/**
+ * @brief Retorna o elemento (x,y) de mat
+ * 
+ * @param mat Ponteiro para a matriz
+ * @param x Linha do elemento
+ * @param y Coluna do elemento
+ * @return double Valor do elemento
+ */
+double leElemento (mat_tipo * mat, int x, int y);
+
+/**
+ * @brief Faz uma copia de src em dst
+ * 
+ * @param src Ponteiro para a matriz fonte
+ * @param dst Ponteiro para a matriz destino
+ * @param dst_id Identificador da matriz destino
+ */
+void copiaMatriz(mat_tipo *src, mat_tipo * dst, int dst_id);
+
+/**
+ * @brief Soma as matrizes a e b e armazena o resultado em c
+ * 
+ * @param a Ponteiro para a matriz a
+ * @param b Ponteiro para a matriz b
+ * @param c Ponteiro para a matriz resultado
+ */
+void somaMatrizes(mat_tipo *a, mat_tipo *b, mat_tipo *c);
+
+/**
+ * @brief Multiplica as matrizes a e b e armazena o resultado em c
+ * 
+ * @param a Ponteiro para a matriz a
+ * @param b Ponteiro para a matriz b
+ * @param c Ponteiro para a matriz resultado
+ */
+void multiplicaMatrizes(mat_tipo *a, mat_tipo *b, mat_tipo *c);
+
+/**
+ * @brief Transpoe a matriz a
+ * 
+ * @param a Ponteiro para a matriz
+ */
+void transpoeMatriz(mat_tipo *a);
+
+/**
+ * @brief Destroi a matriz a, que se torna inacessível
+ * 
+ * @param a Ponteiro para a matriz
+ */
+void destroiMatriz(mat_tipo *a);
