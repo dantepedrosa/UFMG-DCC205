d54654963f54757de1b6313c5b48b29008560002
Dante Pedrosa <dantepedrosa@gmail.com>
2024-11-21 19:07:32 -0300

    Refatoração documentação Doxygen

--- ap5/include/mat.h
+++ ap5/include/mat.h
@@ -1,10 +1,13 @@
-//---------------------------------------------------------------------
-// Arquivo	: mat.h
-// Conteudo	: definicoes do TAD MAT 
-// Autor	: Wagner Meira Jr. (meira@dcc.ufmg.br)
-// Historico	: 2021-10-18 - arquivo criado
-//		: 2021-10-21 - estrutura de diretorios
-//---------------------------------------------------------------------
+/**
+ * @file mat.h
+ * @brief Definitions for the MAT Abstract Data Type (ADT)
+ * 
+ * This file contains the definitions and function prototypes for the MAT ADT.
+ * 
+ * @author Wagner Meira Jr. (meira@dcc.ufmg.br)
+ * @date 2021-10-18
+ * @date 2021-10-21 - directory structure
+ */
 
 #ifndef MATH
 #define MATH
@@ -13,24 +16,133 @@
 #define MAXTAM 5
 #endif
 
-typedef struct mat{
+/**
+ * @struct mat
+ * @brief Structure representing a matrix.
+ * 
+ * This structure represents a matrix with a maximum size of MAXTAM x MAXTAM.
+ * 
+ * @var mat::m
+ * Matrix elements stored in a 2D array.
+ * @var mat::tamx
+ * Number of rows in the matrix.
+ * @var mat::tamy
+ * Number of columns in the matrix.
+ * @var mat::id
+ * Identifier for the matrix.
+ */
+typedef struct mat {
 	double m[MAXTAM][MAXTAM];
 	int tamx, tamy;
 	int id;
 } mat_tipo;
 
+/**
+ * @brief Creates a matrix with specified dimensions and ID.
+ * 
+ * @param mat Pointer to the matrix structure to be initialized.
+ * @param tx Number of rows.
+ * @param ty Number of columns.
+ * @param id Identifier for the matrix.
+ */
 void criaMatriz(mat_tipo * mat, int tx, int ty, int id);
+
+/**
+ * @brief Initializes the matrix with all elements set to zero.
+ * 
+ * @param mat Pointer to the matrix structure to be initialized.
+ */
 void inicializaMatrizNula(mat_tipo * mat);
+
+/**
+ * @brief Initializes the matrix with random elements.
+ * 
+ * @param mat Pointer to the matrix structure to be initialized.
+ */
 void inicializaMatrizAleatoria(mat_tipo * mat);
+
+/**
+ * @brief Accesses the matrix elements.
+ * 
+ * @param mat Pointer to the matrix structure.
+ * @return A double value representing the accessed element.
+ */
 double acessaMatriz(mat_tipo * mat);
+
+/**
+ * @brief Prints the matrix elements.
+ * 
+ * @param mat Pointer to the matrix structure to be printed.
+ */
 void imprimeMatriz(mat_tipo * mat);
-void salvaMatriz(mat_tipo * mat, FILE *);
+
+/**
+ * @brief Saves the matrix to a file.
+ * 
+ * @param mat Pointer to the matrix structure to be saved.
+ * @param file Pointer to the file where the matrix will be saved.
+ */
+void salvaMatriz(mat_tipo * mat, FILE *file);
+
+/**
+ * @brief Writes a value to a specific element in the matrix.
+ * 
+ * @param mat Pointer to the matrix structure.
+ * @param x Row index.
+ * @param y Column index.
+ * @param v Value to be written.
+ */
 void escreveElemento(mat_tipo * mat, int x, int y, double v);
-double leElemento (mat_tipo * mat, int x, int y);
+
+/**
+ * @brief Reads a value from a specific element in the matrix.
+ * 
+ * @param mat Pointer to the matrix structure.
+ * @param x Row index.
+ * @param y Column index.
+ * @return A double value representing the read element.
+ */
+double leElemento(mat_tipo * mat, int x, int y);
+
+/**
+ * @brief Copies the contents of one matrix to another.
+ * 
+ * @param src Pointer to the source matrix structure.
+ * @param dst Pointer to the destination matrix structure.
+ * @param dst_id Identifier for the destination matrix.
+ */
 void copiaMatriz(mat_tipo * src, mat_tipo * dst, int dst_id);
+
+/**
+ * @brief Adds two matrices and stores the result in a third matrix.
+ * 
+ * @param a Pointer to the first matrix structure.
+ * @param b Pointer to the second matrix structure.
+ * @param c Pointer to the result matrix structure.
+ */
 void somaMatrizes(mat_tipo * a, mat_tipo * b, mat_tipo * c);
+
+/**
+ * @brief Multiplies two matrices and stores the result in a third matrix.
+ * 
+ * @param a Pointer to the first matrix structure.
+ * @param b Pointer to the second matrix structure.
+ * @param c Pointer to the result matrix structure.
+ */
 void multiplicaMatrizes(mat_tipo * a, mat_tipo * b, mat_tipo * c);
+
+/**
+ * @brief Transposes the matrix.
+ * 
+ * @param a Pointer to the matrix structure to be transposed.
+ */
 void transpoeMatriz(mat_tipo *a);
+
+/**
+ * @brief Destroys the matrix and frees any allocated resources.
+ * 
+ * @param a Pointer to the matrix structure to be destroyed.
+ */
 void destroiMatriz(mat_tipo *a);
 
 #endif
--- ap5/include/msgassert.h
+++ ap5/include/msgassert.h
@@ -1,12 +1,71 @@
+/**
+ * @file msgassert.h
+ * @brief Assertion macros for error and warning messages.
+ *
+ * This header file provides macros for handling assertions with custom
+ * error and warning messages. It includes macros for issuing warnings
+ * and errors, printing the message to stderr, and optionally aborting
+ * the program on errors.
+ */
+
 #ifndef MSGASSERTH
 #define MSGASSERTH
 
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @def avisoAssert(e, m)
+ * @brief Issues a warning message if the expression is false.
+ *
+ * This macro checks the given expression @p e. If the expression is false,
+ * it calls the internal function __avisoassert to print a warning message
+ * to stderr.
+ *
+ * @param e The expression to evaluate.
+ * @param m The warning message to print if the expression is false.
+ */
 #define avisoAssert(e,m) ((void) ((e)?0: __avisoassert (#e,__FILE__,__LINE__,m)))
+
+/**
+ * @def __avisoassert(e, file, line, m)
+ * @brief Internal function to print a warning message.
+ *
+ * This function prints a warning message to stderr with the file name,
+ * line number, expression, and custom message.
+ *
+ * @param e The expression that was evaluated.
+ * @param file The name of the file where the warning occurred.
+ * @param line The line number where the warning occurred.
+ * @param m The custom warning message.
+ */
 #define __avisoassert(e,file,line,m) ((void) fprintf(stderr,"%s:%u: Aviso: '%s' - %s\n",file,line,e,m),0)
+
+/**
+ * @def erroAssert(e, m)
+ * @brief Issues an error message and aborts the program if the expression is false.
+ *
+ * This macro checks the given expression @p e. If the expression is false,
+ * it calls the internal function __erroassert to print an error message
+ * to stderr and aborts the program.
+ *
+ * @param e The expression to evaluate.
+ * @param m The error message to print if the expression is false.
+ */
 #define erroAssert(e,m) ((void) ((e)?0: __erroassert (#e,__FILE__,__LINE__,m)))
+
+/**
+ * @def __erroassert(e, file, line, m)
+ * @brief Internal function to print an error message and abort the program.
+ *
+ * This function prints an error message to stderr with the file name,
+ * line number, expression, and custom message, and then aborts the program.
+ *
+ * @param e The expression that was evaluated.
+ * @param file The name of the file where the error occurred.
+ * @param line The line number where the error occurred.
+ * @param m The custom error message.
+ */
 #define __erroassert(e,file,line,m) ((void) fprintf(stderr,"%s:%u: Erro '%s' - %s\n",file,line,e,m), abort(),0)
 
 #endif
