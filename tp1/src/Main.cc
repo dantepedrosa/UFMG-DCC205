0074c29ca2579d8cbd0f7d2f02e412cd5f41653b
Dante Pedrosa <dantepedrosa@gmail.com>
2024-11-27 14:34:41 -0300

    Atualiza .gitignore para incluir arquivos .json e adiciona esboço de implementação da classe OrdInd no Main.cc

--- tp1/src/Main.cc
+++ tp1/src/Main.cc
@@ -5,3 +5,35 @@ int main(int argc, char* argv[]) {
     pTest->Print();
     return 0;
 }
+
+/* TODO
+
+#include "OrdInd.h"
+
+int main() {
+    int numRegistros = 5;  // Número de registros na base
+    int numAtributos = 3;  // Nome, CPF, Endereço
+
+    // Cria o objeto de ordenação
+    OrdInd organizador(numRegistros, numAtributos);
+
+    // Carrega os dados
+    if (!organizador.carregaArquivo("entrada.xcsv")) {
+        std::cerr << "Erro ao carregar o arquivo." << std::endl;
+        return 1;
+    }
+
+    // Ordena e imprime as diferentes chaves
+    organizador.ordenaPorNome();
+    organizador.imprimePorNome();
+
+    organizador.ordenaPorCPF();
+    organizador.imprimePorCPF();
+
+    organizador.ordenaPorEndereco();
+    organizador.imprimePorEndereco();
+
+    return 0;
+}
+
+*/
