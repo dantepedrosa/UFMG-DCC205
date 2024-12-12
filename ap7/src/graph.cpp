ea0674de3a0719b2e2ec8a49fa309fa108a8c10d
Dante Pedrosa <dantepedrosa@gmail.com>
2024-12-12 07:46:55 -0300

    Implementação inicial da classe Grafo com métodos básicos

---
+++ ap7/src/graph.cpp
@@ -0,0 +1,75 @@
+#include "graph.hpp"
+
+/**
+ * @brief Construtor da classe Grafo.
+ */
+Grafo::Grafo() {
+    // TODO: Implementar o construtor
+}
+
+/**
+ * @brief Destrutor da classe Grafo.
+ */
+Grafo::~Grafo() {
+    // TODO: Implementar o destrutor
+}
+
+/**
+ * @brief Insere um novo vértice no grafo.
+ */
+void Grafo::InsereVertice() {
+    // TODO: Implementar a inserção de um vértice
+}
+
+/**
+ * @brief Insere uma nova aresta entre os vértices v e w.
+ * @param v Vértice de origem.
+ * @param w Vértice de destino.
+ */
+void Grafo::InsereAresta(int v, int w) {
+    // TODO: Implementar a inserção de uma aresta
+}
+
+/**
+ * @brief Retorna a quantidade de vértices no grafo.
+ * @return Quantidade de vértices.
+ */
+int Grafo::QuantidadeVertices() {
+    // TODO: Implementar a contagem de vértices
+    return 0;
+}
+
+/**
+ * @brief Retorna a quantidade de arestas no grafo.
+ * @return Quantidade de arestas.
+ */
+int Grafo::QuantidadeArestas() {
+    // TODO: Implementar a contagem de arestas
+    return 0;
+}
+
+/**
+ * @brief Retorna o grau mínimo dos vértices no grafo.
+ * @return Grau mínimo.
+ */
+int Grafo::GrauMinimo() {
+    // TODO: Implementar a obtenção do grau mínimo
+    return 0;
+}
+
+/**
+ * @brief Retorna o grau máximo dos vértices no grafo.
+ * @return Grau máximo.
+ */
+int Grafo::GrauMaximo() {
+    // TODO: Implementar a obtenção do grau máximo
+    return 0;
+}
+
+/**
+ * @brief Imprime os vizinhos do vértice v.
+ * @param v Vértice cujos vizinhos serão impressos.
+ */
+void Grafo::ImprimeVizinhos(int v) {
+    // TODO: Implementar a impressão dos vizinhos
+}
